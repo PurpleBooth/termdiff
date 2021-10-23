0f0fc84f615e5c8c065941969f451f371551003b
Billie Thompson <billie@billiecodes.com>
2021-10-23 14:22:40 +0200

    refactor: Create a simple color theme

--- src/output/plain.rs
+++ src/output/plain.rs
@@ -1,35 +1,73 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    borrow::Cow,
+    fmt::{Display, Formatter},
+};
 
+use crossterm::style::Stylize;
 use similar::{ChangeTag, DiffableStr, TextDiff};
 
 use super::protocol::Output;
 
+type LineFormatter = fn(Cow<str>) -> String;
+
+pub struct Theme {
+    equal_line: LineFormatter,
+    delete_line: LineFormatter,
+    insert_line: LineFormatter,
+    line_end: String,
+}
+
+fn colorless_theme() -> Theme {
+    Theme {
+        equal_line: |line| format!(" {}", line),
+        delete_line: |line| format!("<{}", line),
+        insert_line: |line| format!(">{}", line),
+        line_end: "\n".into(),
+    }
+}
+fn color_theme() -> Theme {
+    Theme {
+        equal_line: |line| format!(" {}", line),
+        delete_line: |line| format!("<{}", line).red().to_string(),
+        insert_line: |line| format!(">{}", line).green().to_string(),
+        line_end: "\n".into(),
+    }
+}
+
 pub struct Plain<'a, T: DiffableStr + ?Sized> {
     diff: TextDiff<'a, 'a, 'a, T>,
-    equal_sign: String,
-    delete_sign: String,
-    insert_sign: String,
+    theme: Theme,
 }
 
 impl<'a, T: DiffableStr + ?Sized> Plain<'a, T> {
-    fn prefix(&self, tag: ChangeTag) -> String {
+    pub(crate) fn new(diff: TextDiff<'a, 'a, 'a, T>, theme: Theme) -> Plain<'a, T> {
+        Plain { diff, theme }
+    }
+}
+
+impl<'a, T: DiffableStr + ?Sized> Plain<'a, T> {
+    fn format_line(&self, line: Cow<str>, tag: ChangeTag) -> String {
         match tag {
-            ChangeTag::Equal => self.equal_sign.clone(),
-            ChangeTag::Delete => self.delete_sign.clone(),
-            ChangeTag::Insert => self.insert_sign.clone(),
+            ChangeTag::Equal => (self.theme.equal_line)(line),
+            ChangeTag::Delete => (self.theme.delete_line)(line),
+            ChangeTag::Insert => (self.theme.insert_line)(line),
         }
     }
 }
 
-impl<T: DiffableStr + ?Sized> Display for Plain<'_, T> {
+impl<'a, T: DiffableStr + ?Sized> Display for Plain<'a, T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         for change in self.diff.iter_all_changes() {
             for line in change.value().tokenize_lines() {
-                write!(f, "{}{}", self.prefix(change.tag()), line.to_string_lossy())?;
+                write!(
+                    f,
+                    "{}",
+                    self.format_line(line.to_string_lossy(), change.tag())
+                )?;
             }
 
             if change.missing_newline() {
-                writeln!(f)?;
+                write!(f, "{}", self.theme.line_end)?;
             }
         }
 
@@ -41,9 +79,7 @@ impl<'a, T: DiffableStr + ?Sized> From<TextDiff<'a, 'a, 'a, T>> for Plain<'a, T>
     fn from(diff: TextDiff<'a, 'a, 'a, T>) -> Self {
         Self {
             diff,
-            equal_sign: " ".to_string(),
-            insert_sign: ">".to_string(),
-            delete_sign: "<".to_string(),
+            theme: colorless_theme(),
         }
     }
 }
@@ -54,7 +90,8 @@ impl<'a, T: DiffableStr + ?Sized> Output<TextDiff<'a, 'a, 'a, T>> for Plain<'a, 
 mod test {
     use similar::TextDiff;
 
-    use crate::output::plain::Plain;
+    use super::color_theme;
+    use crate::output::Plain;
 
     #[test]
     fn single_characters() {
@@ -81,6 +118,18 @@ mod test {
         assert_eq!(
             format!("{}", actual),
             "<Good Error\n<Bad Success\n>Bad Error\n>Good Success\n"
+        );
+    }
+
+    #[test]
+    fn its_customisable() {
+        let old = "Good Error\nBad Success";
+        let new = "Bad Error\nGood Success";
+        let actual: Plain<_> = Plain::new(TextDiff::from_lines(old, new), color_theme());
+
+        assert_eq!(
+            format!("{}", actual),
+            "\u{1b}[38;5;9m<Good Error\n\u{1b}[39m\u{1b}[38;5;9m<Bad Success\u{1b}[39m\n\u{1b}[38;5;10m>Bad Error\n\u{1b}[39m\u{1b}[38;5;10m>Good Success\u{1b}[39m\n"
         );
     }
 
